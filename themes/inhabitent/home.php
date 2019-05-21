34a17e25bc730b62ec1b7dac3e636120f63498f8
mizzta <ritatung@hotmail.com>
2019-05-21 09:59:57 -0700

    add inhabitent plugin

--- themes/inhabitent/home.php
+++ themes/inhabitent/home.php
@@ -11,31 +11,21 @@ get_header(); ?>
 <!-- <?php the_content(); ?> -->
 
 <!-- -------------------------------------- -->
-<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-			<?php endwhile; // End of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<!-- -------------------------------------- -->
 <div class="fullpage">
    <div class="content">
       <?php if (have_posts()) : ?>
 
          <!-- The wordpress loop -->
          <?php while (have_posts()) : the_post(); ?>
-
+            
+            <div class="title-meta-background">
             <!-- Template tags -->
             <?php the_post_thumbnail('large', array('class' => 'feature-image')); ?>
             <!-- feature image -->
             <h2 class="post-title"><?php the_title(); ?></h2>
             <p><?php the_author(); ?></p>
+            </div> <!--title-meta-background -->
+            
             <p><?php the_excerpt(); ?></p>
 
             <a href="<?php the_permalink(); ?>">Read more</a><br />
