760eba3b507dcf4772b334b796c6ac7f3fe94658
mizzta <ritatung@hotmail.com>
2019-05-23 14:06:08 -0700

    add 2-columns layout template file.

--- themes/inhabitent/template-parts/page_two-columns.php.php
+++ themes/inhabitent/template-parts/page_two-columns.php.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * Template Name: Two Columns
+ * Template Name: Two Columns Layout
+ * Template Post Type: post, page
  *
  * @package Inhabitent_Theme
  */
@@ -8,26 +9,27 @@
 get_header(); ?>
 
 <div class="fullpage">
-   <div class="content">
-<!-- -------------------------------------- -->
+	<div class="content">
+		<!-- -------------------------------------- -->
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+				<?php while (have_posts()) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+					<?php get_template_part('template-parts/content', 'page'); ?>
 
-			<?php endwhile; // End of the loop. ?>
+				<?php endwhile;
+			?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+			</main><!-- #main -->
+		</div><!-- #primary -->
+	</div><!-- .content -->
 
+	<aside>
+		<?php get_sidebar(); ?>
+	</aside>
 
-<aside>
-      <?php get_sidebar(); ?>
-   </aside>
-   
 </div><!-- .fullpage -->
 
 <!-- -------------------------------------- -->
