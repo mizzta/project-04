111132176748c9337f60a14c873141fe69054d63
mizzta <ritatung@hotmail.com>
2019-06-05 13:27:02 -0700

    add inhabitent new theme.

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -14,8 +14,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
-
 
 	<?php wp_head(); ?>
 </head>
@@ -24,28 +22,27 @@
 	<div id="page" class="hfeed site">
 		<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html('Skip to content'); ?></a>
 
-		<header id="masthead" class="site-header container" role="banner">
+		<header id="masthead" class="site-header" role="banner">
+		<div class="container">
+			<div class="site-branding">
+				<a href="<?php echo get_home_url(); ?>" class="logos">
+					<img class="site-logo-white" src="<?php echo get_template_directory_uri(); ?>/assets/images/logos/inhabitent-logo-tent-white.svg" />
+					<img class="site-logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/logos/inhabitent-logo-tent.svg" />
+				</a>
+				<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+				<!-- <p class="site-description"><?php bloginfo('description'); ?></p> -->
+			</div><!-- .site-branding -->
 
-				<div class="site-branding">
-					<a href="<?php echo get_home_url(); ?>">
-						<img class="site-logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/logos/inhabitent-logo-tent-white.svg" />
-					</a>
-
-					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
-					<p class="site-description"><?php bloginfo('description'); ?></p>
-				</div><!-- .site-branding -->
-
-				<nav id="site-navigation" class="main-navigation" role="navigation">
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<div class="nav-main">
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html('Primary Menu'); ?></button>
 					<?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu')); ?>
-
 					<div class="header-search">
 						<?php get_search_form(); ?>
 					</div>
-
-				</nav><!-- #site-navigation -->
-
+				</div>
+			</nav><!-- #site-navigation -->
+		</div>
 		</header><!-- #masthead -->
-
 
 		<div id="content" class="site-content">
